7f0f4aca82499ea6985f883b6c873281de343b93
mildaslepaviciute <milda.slepaviciute@gmail.com>
2020-02-27 21:10:28 +0200

    no message

--- js/animate.js
+++ js/animate.js
@@ -1,17 +1,33 @@
 $(document).ready(function() {
 
-    $('.js--wp-2').waypoint(function(direction) {
-      $('.js--wp-2').addClass('animated fadeInLeft');
+    $('.js--wp-4').waypoint(function(direction) {
+      $('.js--wp-4').addClass('animated fadeInLeft');
     }, {
       offset: '80%'
     })
 
-    $('.js--wp-3').waypoint(function(direction) {
-      $('.js--wp-3').addClass('animated pulse');
+    $('.js--wp-5').waypoint(function(direction) {
+      $('.js--wp-5').addClass('animated pulse');
     }, {
       offset: '90%'
     })
 
+    $('.js--wp-1').waypoint(function(direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+      offset: '100%'
+    })
 
+    $('.js--wp-2').waypoint(function(direction) {
+      $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+      offset: '100%'
+    })
+
+    $('.js--wp-3').waypoint(function(direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+      offset: '100%'
+    })
 
 });
